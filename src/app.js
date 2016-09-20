31c94899d926601d2d8c668f4b8bddc3ba2c551f
alex-pro <a.partitskiy@gmail.com>
2016-09-20 14:29:18 +0300

    starting with react

---
+++ src/app.js
@@ -0,0 +1,12 @@
+var App = React.createClass({
+  render: function() {
+    return (
+      <h1>Magic ball</h1>
+    );
+  }
+});
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
