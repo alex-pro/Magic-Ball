635b80303e7625afffb56e20cf2bbfa343ee1cae
alex-pro <a.partitskiy@gmail.com>
2016-12-08 15:38:01 +0200

    improve usability

--- js/app.js
+++ js/app.js
@@ -1,27 +1,37 @@
 var App = React.createClass({
+  componentDidMount() {
+    var question = document.getElementById('question');
+    question.focus();
+    question.setAttribute('autocomplete', 'off');
+  },
+
   sleep(func, wait) {
     return setTimeout(function() {
       return func();
     }, wait);
   },
 
+  currentAnswers() {
+    return document.getElementById('answers');
+  },
+
   newAnswer(value) {
     var li = document.createElement('li');
     li.className = 'answer';
-    li.innerHTML = value  ;
+    li.innerHTML = value;
     return li;
   },
 
   getAnswer() {
     var answer = answers[parseInt(Math.random() * (answers.length))];
-    document.getElementById('answers').insertBefore(this.newAnswer(answer), document.getElementById('answers').firstChild);
-    document.getElementById('question').value = '';
+    this.currentAnswers().insertBefore(this.newAnswer(answer), this.currentAnswers().firstChild);
+    question.value = '';
   },
 
   shake(event) {
     event.preventDefault();
     var action = actions[parseInt(Math.random() * (actions.length))];
-    document.getElementById('answers').insertBefore(this.newAnswer(action), document.getElementById('answers').firstChild);
+    this.currentAnswers().insertBefore(this.newAnswer(action), this.currentAnswers().firstChild);
     this.sleep(this.getAnswer, 2000);
   },
 
@@ -30,7 +40,7 @@ var App = React.createClass({
       <div>
         <h1>Спрашивай, не стесняйся!</h1>
         <form onSubmit={this.shake}>
-          <input id='question' type='text' autofocus />
+          <input id='question' type='text' />
         </form>
         <ul id='answers' className='answers'>
         </ul>
@@ -43,3 +53,8 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+var question = document.getElementById('question');
+question.addEventListener('blur', function( event ) {
+  event.target.focus();
+}, true);
